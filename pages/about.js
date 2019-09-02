476692016865f288e1eec531e5b2e49bbfbe832d
Diego Ortiz <alejojo23@gmail.com>
2019-09-01 22:37:24 -0500

    Challenge video #3 about creating a new page using styles an a static image

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import css from './about.scss'
+
+const About = () => (
+    <>
+        <h2 className={css.title}>This is About page</h2>
+        <p>
+            <img src="/static/images/podcast.jpg" />
+        </p>
+    </>
+)
+
+export default About
