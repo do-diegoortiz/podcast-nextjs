6c721d86d0aa105fc0eb5401d0bf8d2863165bdb
Diego Ortiz <alejojo23@gmail.com>
2019-09-01 22:22:25 -0500

    Created basic NextJS architecture. Added style setup and js linters

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import css from './index.scss'
+
 const Index = () => (
     <>
-        <h1>Hello World!</h1>
+        <h1 className={css.example}>Hello World!</h1>
         <p>Welcome to Next.js Course</p>
-    </> 
+    </>
 )
 
 export default Index
