258587d26774acc7b73b2ced80eb1841a775c42f
Diego Ortiz <alejojo23@gmail.com>
2019-09-02 23:06:13 -0500

    getInitialProps and channel style in video #5

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-`yarn next dev` to run the app locally
+Use `yarn next dev` or `yarn dev` commands to run the app locally
+
+## getInitialProps()
+
+We can use `getInitialProps()` only inside pages, it's part of NextJS
+The `fetch` method doesn't exist in NextJS, we need to add library `isomorphic-fetch`
+
+    yarn add isomorphic-fetch
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
 import css from './index.scss'
+import 'isomorphic-fetch'
 
-const Index = () => (
-    <>
-        <h1 className={css.example}>Hello World!</h1>
-        <p>Welcome to Next.js Course</p>
-    </>
-)
+class Index extends React.Component {
+
+    static async getInitialProps() {
+        const req = await fetch('https://api.audioboom.com/channels/recommended')
+        const { body: channels } = await req.json();
+        return { channels };
+    }
+
+    render() {
+        const { channels } = this.props
+        // short for: const channels = this.props.channels
+
+        const channelsList = channels.map(channel => (
+            <div className={css.channelContainer}>
+                <img className={css.channelImg} src={channel.urls.logo_image.original} />
+                <h2 className={css.channelTitle}> {channel.title} </h2>
+            </div>
+        ))
+
+        return <>
+            <header className={css.title}>Podcasts</header>
+            <div className={css.channels}>
+                {channelsList}
+            </div>
+        </>
+    }
+}
 
 export default Index
